991bd775de03f37ed9df7531ff29fd7148ec9a3b
fokin <jr3146990848@gmail.com>
2025-07-15 21:50:27 +0800

    finish timing the getLast method of SLList

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,44 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
-    }
+        // finish N and # ops
+        AList<SLList> arraySLList = new AList<>();
+        AList<Integer> N = new AList<>();
+        AList<Integer> ops = new AList<>();
+        int firstSize = 1000;
+        int callTimes = 10000;
+        int testTimes = 8;
+        int generalValue = 1;
+        for (int i = 0; i < testTimes; i++) {
+            // (create 8 ALists for test)
+            SLList<Integer> innerList = new SLList<>();
+            arraySLList.addLast(innerList);
+            N.addLast(firstSize);
+            ops.addLast(callTimes);
+            firstSize = firstSize * 2;
+        }
 
+        // fill arraySLList
+        AList<Double> time = new AList<>();
+        for (int i = 0; i < testTimes; i++) {
+            SLList<Integer> current = arraySLList.get(i);
+            for (int j = 0; j < N.get(i); j++) {
+                current.addLast(generalValue);
+            }
+        }
+
+        // test getLast
+        for (int i = 0; i < testTimes; i++) {
+            SLList<Integer> testList = arraySLList.get(i);
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < callTimes; j++) {
+                int value = testList.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            time.addLast(timeInSeconds);
+        }
+
+        // put them to printTimingTable()
+        printTimingTable(N, time, ops);
+    }
 }
