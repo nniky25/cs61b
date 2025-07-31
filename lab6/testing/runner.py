cebbc2ec81c9b11d39acfbded0fcd5eb0862d083
fokin <jr3146990848@gmail.com>
2025-07-31 16:52:35 +0800

    fix runner.py

--- lab6/testing/runner.py
+++ lab6/testing/runner.py
@@ -441,8 +441,7 @@ if __name__ == "__main__":
                 DEBUG = True
                 TIMEOUT = 100000
         if lib_dir is None:
-            lib_dir = join(abspath(environ['REPO_DIR']),
-                           "library-sp21/javalib")
+            lib_dir = join("/Users/li/cs/cs61B/cs61b/library-sp21", "javalib")
         else:
             lib_dir = join(abspath(getcwd()), abspath(lib_dir))
     except GetoptError:
